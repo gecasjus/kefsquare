8b526d001e34d49466fea1718d0bd06b7c5d3575
Justas <Justas@Justass-MacBook-Air.local>
2020-10-23 13:34:59 +0300

    favicon update

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -38,7 +38,7 @@ class Register extends React.Component {
 				this.props.loadUser(user);
 				this.props.onRouteChange('home')
 			} else {
-				alert('wrong input');
+				alert('Something went wrong!');
 			}
 		})
 	} 
